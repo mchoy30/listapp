dd8a3589cf36975fa3b3cf9178f3fca4273430a5
Mackenzie Choy <mchoy30@academic.rrc.ca>
2019-05-14 18:37:11 -0500

    Made app into a smart component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+  class App extends React.Component {
+    render()
+    {
+      return (
+          
+        <>
+       <p>hellp</p>
+       <p>asdsad</p>
+       </>
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      );
+    }
+
+
 }
 
 export default App;
